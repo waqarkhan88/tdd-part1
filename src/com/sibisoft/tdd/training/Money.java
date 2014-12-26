20770a6c1434bc4341f62dd031871265f628e4e7
Waqar Ali Khan <waqar.khan17@gmail.com>
2014-12-26 22:37:49 +0500

    Code uptill 11 chapters
    Completed writing code of 11 chapters

--- src/com/sibisoft/tdd/training/Money.java
+++ src/com/sibisoft/tdd/training/Money.java
@@ -1,23 +1,39 @@
 package com.sibisoft.tdd.training;
 
-public abstract class Money {
+public class Money {
 	
 	protected int amount;
+	protected String currency;
 	
-	public abstract Money times(int multiplier);
+	public Money(int amount, String currency) {
+		this.amount = amount;
+		this.currency = currency;
+	}
+	
+	public String currency() {
+		return currency;
+	}
+
+	public Money times(int multiplier) {
+		return new Money(amount * multiplier, currency);
+	}
 	
 	public boolean equals(Object object) {
-		Money money= (Money) object;
+		Money money = (Money) object;
 		return amount == money.amount
-				&& getClass().equals(money.getClass());
+				&& currency().equals(money.currency());
 	}
 	
 	public static Money dollar(int amount) {
-		return new Dollar(amount);
+		return new Money(amount, "USD");
 	}
 	
 	public static Money franc(int amount) {
-		return new Franc(amount);
+		return new Money(amount, "CHF");
+	}
+	
+	public String toString() {
+		return amount + " " + currency;
 	}
 	
 }
