27dbaa67963271d05a9d5aabe29d4a67bc842d95
Waqar Ali Khan <waqar.khan17@gmail.com>
2014-12-29 18:51:27 +0500

    Code up-to Chapter# 16
    Implementation of code upto chapter# 16.

--- src/com/sibisoft/tdd/training/Expression.java
+++ src/com/sibisoft/tdd/training/Expression.java
@@ -3,5 +3,7 @@ package com.sibisoft.tdd.training;
 public interface Expression {
 	
 	public Money reduce(Bank bank, String to);
+	public Expression plus(Expression addend);
+	public Expression times(int multiplier);
 
 }
